699e084c66705d2c31c771b3091349b487700711
Radhika Canopas <radhika.s@canopas.com>
2023-10-31 14:31:05 +0530

    Cleanup

--- editor/src/main/java/com/canopas/editor/ui/ui/RichTextField.kt
+++ editor/src/main/java/com/canopas/editor/ui/ui/RichTextField.kt
@@ -7,19 +7,11 @@ import android.view.ViewGroup
 import android.view.accessibility.AccessibilityEvent
 import android.widget.EditText
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextRange
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.widget.doAfterTextChanged
 import com.canopas.editor.ui.data.RichTextState
@@ -30,18 +22,6 @@ import com.canopas.editor.ui.data.RichTextState
 internal fun RichTextField(
     value: RichTextState,
     modifier: Modifier = Modifier,
-    enabled: Boolean = true,
-    readOnly: Boolean = false,
-    textStyle: TextStyle = TextStyle.Default,
-    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
-    keyboardActions: KeyboardActions = KeyboardActions.Default,
-    singleLine: Boolean = false,
-    maxLines: Int = if (singleLine) 1 else Int.MAX_VALUE,
-    onTextLayout: (TextLayoutResult) -> Unit = {},
-    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-    cursorBrush: Brush = SolidColor(Color.Black),
-    decorationBox: @Composable (innerTextField: @Composable () -> Unit) -> Unit =
-        @Composable { innerTextField -> innerTextField() }
 ) {
 
     val context = LocalContext.current
